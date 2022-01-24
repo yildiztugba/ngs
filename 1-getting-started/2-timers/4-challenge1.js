ec978eb35073bc16368d15fff4e85fca573a82f6
yildiztugba <okra.yildiz@gmail.com>
2022-01-24 10:54:25 +0100

    done

--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -1,9 +1,9 @@
-const theOneFunc = () => {};
-
-setTimeout(theOneFunc, 4 * 1000);
+const theOneFunc = (seconds) => {
+    console.log("Hello after", seconds, "seconds");
+};
 
 // Hello after 4 seconds
-
+setTimeout(theOneFunc, 4 * 1000,"4");
 // Hello after 8 seconds
+setTimeout(theOneFunc, 8 * 1000,"8");
 
-// You can define only ONE function
