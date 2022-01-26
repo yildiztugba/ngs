207ed9f6baf66441950a7e974181010b4c71d0a0
yildiztugba <okra.yildiz@gmail.com>
2022-01-26 11:14:47 +0100

    http,server

--- 5-web/1-hello-world.js
+++ 5-web/1-hello-world.js
@@ -1,9 +1,16 @@
-const http = require('http');
+const http = require('http'); // return api of http module in to local variable
 
-const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
-});
+const requestListener = (req, res) => { 
+  res.write('Hello World\n')
+  res.end();
+};
+
+
+//createServer => higher order function (receive other function as an argument)
+const server = http.createServer();
+
+server.on('request', requestListener); //for every request
 
 server.listen(4242, () => {
-  console.log('Server is running...');
+  console.log('Server is running...'); // if suscess, callback is working
 });
