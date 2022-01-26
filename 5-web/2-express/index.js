cc14ae9058568e9b4ef65374e63e5ade2f9f89be
yildiztugba <okra.yildiz@gmail.com>
2022-01-26 11:35:14 +0100

    added server path

--- 5-web/2-express/index.js
+++ 5-web/2-express/index.js
@@ -1,6 +1,17 @@
-const express = require('express');
+//other framework ; koajs, sailjs, meteor besides espress
 
-const server = express();
+const express = require('express'); // return express now a function.
+
+const server = express(); 
+
+server.get('/', (req,res)=> {
+res.send('Hello Express');
+})
+
+server.get('/about', (req,res)=> {
+  res.send('About...');
+  })
+  
 
 server.listen(4242, () => {
   console.log('Express Server is running...');
